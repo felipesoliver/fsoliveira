29be21f82b77de5d7d622ff651aaa3dbddb349f0
Felipe Oliveira <felipephito@gmail.com>
2020-10-13 16:22:42 -0300

    Created a .js file to separate scripts from .html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+    document.getElementById("cabecalhoPrincipal").style.opacity = "1";
+    document.getElementById("cabecalhoPrincipal").style.transition = "all 0.6s ease-in-out";
+    } else {
+    document.getElementById("cabecalhoPrincipal").style.opacity = "0";
+    }
+    prevScrollpos = currentScrollPos;
+}
